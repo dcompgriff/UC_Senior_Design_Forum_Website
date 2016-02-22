faaa357be60408525f10d9a8c823c8a6c7c90a63
Dan Griffin <dcompgriff@gmail.com>
2016-02-22 18:42:06 -0500

    Added new js code to load project list, and to parse the degree program sidebar.

--- uc_senior_design/project_boards/models.py
+++ uc_senior_design/project_boards/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -57,18 +59,18 @@ member_list='FName, SName, TName', advisor='Dr man', future_work='Awesome future
 topic='mtopic', title='mtitle', year='2016')
 '''
 
+testProject1 = Project(board_image_url='/myimage.jpg', abstract='In this paper we investigate the use of the area under the receiver operating characteristic (ROC) curve (AUC) as a performance measure for machine learning algorithms. As a case study we evaluate six machine learning algorithms (C4.5, Multiscale Classifier, Perceptron, Multi-layer Perceptron, k-Nearest Neighbours, and a Quadratic Discriminant Function) on six “real world” medical diagnostics data sets. We compare and discuss the use of AUC to the more conventional overall accuracy and find that AUC exhibits a number of desirable properties when compared to overall accuracy: increased sensitivity in Analysis of Variance (ANOVA) tests; a standard error that decreased as both AUC and the number of test samples increased; decision threshold independent; and it is invariant to a priori class probabilities. The paper concludes with the recommendation that AUC be used in preference to overall accuracy for “single number” evaluation of machine learning algorithms.', member_list='FName, SName, TName', advisor='Dr man', future_work='Vinith Misra (Stanford University, USA); Tsachy Weissman (Stanford University, USA) This paper was about universal decoding, sort of. THe idea is that the decoder doesn’t know the codebook but it knows the encoder is using a random block code. However, it doesn’t know the rate, even. The question is really what can one say in this setting? For example, symmetry dictates that the actual message label will be impossible to determine, so the error criterion has to be adjusted accordingly. The decoding strategy that they propose is a partition of the output space (or “clustering”) followed by a labeling. They claim this is a model for clustering through an information theoretic lens, but since the number of clusters is exponential in the dimension of the space, I think that it’s perhaps more of a special case of clustering. A key concept in their development is something they call the minimum partition information, which takes the place of the maximum mutual information (MMI) used in universal decoding (c.f. Csiszr and Krner).',topic='Machine Learning', title='Smart Connected Health 1', year='2016')
+testProject2 = Project(board_image_url='/myimage.jpg', abstract='In this paper we investigate the use of the area under the receiver operating characteristic (ROC) curve (AUC) as a performance measure for machine learning algorithms. As a case study we evaluate six machine learning algorithms (C4.5, Multiscale Classifier, Perceptron, Multi-layer Perceptron, k-Nearest Neighbours, and a Quadratic Discriminant Function) on six “real world” medical diagnostics data sets. We compare and discuss the use of AUC to the more conventional overall accuracy and find that AUC exhibits a number of desirable properties when compared to overall accuracy: increased sensitivity in Analysis of Variance (ANOVA) tests; a standard error that decreased as both AUC and the number of test samples increased; decision threshold independent; and it is invariant to a priori class probabilities. The paper concludes with the recommendation that AUC be used in preference to overall accuracy for “single number” evaluation of machine learning algorithms.', member_list='FName, SName, TName', advisor='Dr man', future_work='Vinith Misra (Stanford University, USA); Tsachy Weissman (Stanford University, USA) This paper was about universal decoding, sort of. THe idea is that the decoder doesn’t know the codebook but it knows the encoder is using a random block code. However, it doesn’t know the rate, even. The question is really what can one say in this setting? For example, symmetry dictates that the actual message label will be impossible to determine, so the error criterion has to be adjusted accordingly. The decoding strategy that they propose is a partition of the output space (or “clustering”) followed by a labeling. They claim this is a model for clustering through an information theoretic lens, but since the number of clusters is exponential in the dimension of the space, I think that it’s perhaps more of a special case of clustering. A key concept in their development is something they call the minimum partition information, which takes the place of the maximum mutual information (MMI) used in universal decoding (c.f. Csiszr and Krner).',topic='Machine Learning', title='Smart Connected Health 2', year='2016')
+testProject3 = Project(board_image_url='/myimage.jpg', abstract='This is the abstract', member_list='First Person, Second Person, Third Person, Fourth Person', advisor='Dr man', future_work='Awesome future projects',topic='mtopic', title='mtitle', year='2015')
 
 
 
-
-
-
-
-
-
-
-
-
+degreeProgram1 = DegreeProgram(degree_program_name='electrical_engineering_and_computing_systems')
+degreeProgram2 = DegreeProgram(degree_program_name='aerospace_engineering_and_engineering_mechanics')
+degreeProgram3 = DegreeProgram(degree_program_name='civil_and_architectural_engineering_and_construction')
+degreeProgram4 = DegreeProgram(degree_program_name='biomedical_chemical_and_environmental_engineering')
+degreeProgram5 = DegreeProgram(degree_program_name='engineering_education')
+degreeProgram6 = DegreeProgram(degree_program_name='mechanical_and_materials_engineering')
 
 
 
--- uc_senior_design/static/js/senior_design_base.js
+++ uc_senior_design/static/js/senior_design_base.js
@@ -1,4 +1,38 @@
-$(document).on("click", ".list-group-item", function(){
-     $(".list-group-item").removeClass("active");
-     $(this).addClass("active");
+$(document).ready(function(){
+
+    
+    //JQuery code to retrieve the project list for the current program and year.
+    $("#get_projects").click(function(){
+        $("#project_list").load('../projects/cmpe/2016/');
+    });
+    $("#remove_projects").click(function(){
+        $("#project_list").empty();
+    });
+
+    //Simulate an initial click on the program list to load data.
+    $(".list-group-item.program").first().click();
+
+});
+
+
+//Code for the program selection list.
+$(document).on("click", ".list-group-item.program", function(){
+    //JQuery code to update the program selected.
+    $(".list-group-item.program").removeClass("active");
+    $(this).addClass("active");
+
+    //JQuery to retrieve the currenlty active panel, and to 
+    var degree_program = $(this).text()
+    //Set degree program name to lowercase.
+    degree_program = degree_program.toLowerCase()
+    degree_program_sentence_list = degree_program.split(" ")
+    degree_program = ""
+    //Remove spaces, and add an underscore.
+    for(i = 0; i< degree_program_sentence_list.length; i++){
+        degree_program += degree_program_sentence_list[i] + "_"
+    }
+    //Remove the last underscore added by the loop.
+    degree_program = degree_program.slice(0, degree_program.length - 1)
+    alert(degree_program)
+
 });
