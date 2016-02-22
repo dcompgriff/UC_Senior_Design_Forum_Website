6e426dc8f6bbd72db6a0762838ad8d944e77cda9
Dan Griffin <dcompgriff@gmail.com>
2016-02-22 00:12:49 -0500

    Added css animations and updated project.html template.

--- uc_senior_design/project_boards/views.py
+++ uc_senior_design/project_boards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse, HttpResponseNotFound
 
-from .models import Project
+from .models import Project, DegreeProgram
 
 # Create your views here.
 
