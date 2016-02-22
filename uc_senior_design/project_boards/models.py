fb9cc4332f292fe1d704e5c1530927a993858cef
Dan Griffin <dcompgriff@gmail.com>
2016-02-21 22:19:11 -0500

    Updated database models and added some initial data to the db.

--- uc_senior_design/project_boards/models.py
+++ uc_senior_design/project_boards/models.py
@@ -1,29 +1,42 @@
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
-
-# Create your models here.
-
-'''
-Object that stores information about the year associated with a set of 
-senior design projects.
-'''
-class Year(models.Model):
-    class_year = models.DateField()
     
 
 '''
 Object that stores information about the degree program for 
 a set of senior design projects. 
 '''
+@python_2_unicode_compatible 
 class DegreeProgram(models.Model):
+    
+    def __str__(self):
+        return unicode(self.degree_program_name)
+    
     degree_program_name = models.TextField()
 
 '''
 Model describing a single senior design project. This data is displayed 
 as an individual project with data.
 '''
+@python_2_unicode_compatible 
 class Project(models.Model):
+    
+    def __str__(self):
+        return unicode(self.log_form())
+        
+    def log_form(self):
+        niceOutputString = ''
+        niceOutputString += self.board_image_url + '\n'
+        niceOutputString += self.abstract + '\n'
+        niceOutputString += self.member_list + '\n'
+        niceOutputString += self.advisor + '\n'
+        niceOutputString += self.future_work + '\n'
+        niceOutputString += self.topic + '\n'
+        niceOutputString += self.title + '\n'
+        
+    
     #Basic information about a project.
     board_image_url = models.TextField()
     abstract = models.TextField()
@@ -32,15 +45,18 @@ class Project(models.Model):
     future_work = models.TextField()
     topic = models.TextField()
     title = models.TextField()
-    
-    #Year Reference.
-    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    year = models.TextField()
+
     #Engineering Class Reference. The CASCADE ensures that if a year is removed, then so are all of the projects for that year.
     degree_program = models.ForeignKey(DegreeProgram, on_delete=models.CASCADE)
 
 
 
-
+'''
+Project(board_image_url='/myimage.jpg', abstract='This is the abstract', 
+member_list='FName, SName, TName', advisor='Dr man', future_work='Awesome future projects',
+topic='mtopic', title='mtitle', year='2016')
+'''
 
 
 
